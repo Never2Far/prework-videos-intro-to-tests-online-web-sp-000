4e89ff2b2de7942285eddef1c96a8301baf3c589
Seth Near <seth.j.near@gmail.com>
2020-05-26 18:29:52 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -4,7 +4,7 @@ puts "Tests are running..."
 
 ruby describe "conversions" do
   describe '#ounces_to_grams' do
- it 'given zero, returns 0.0' do 
+ it 'given zero, returns 0.0' do
    grams = ounces_to_grams(0) expect(grams).to eq(0.0)
   end
 end
