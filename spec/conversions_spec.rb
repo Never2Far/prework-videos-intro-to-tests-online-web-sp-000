1c8857112175d5e715672ffa88a33e037d97b30d
Seth Near <seth.j.near@gmail.com>
2020-05-26 18:51:48 +0000

    Automatically backed up by Learn

--- spec/conversions_spec.rb
+++ spec/conversions_spec.rb
@@ -1,8 +1,8 @@
-ruby require_relative '../conversions.rb'
+require_relative '../conversions.rb'
 
 puts "Tests are running..."
 
-describe "conversions" do
+ruby describe "conversions" do
   describe '#ounces_to_grams' do
     it 'given zero, returns 0.0' do
       grams = ounces_to_grams(0)
